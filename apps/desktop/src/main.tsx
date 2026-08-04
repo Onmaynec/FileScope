f0f119ae4072b702ea7daf5b551852afe0e0b6d3
Kufik <127012125+Onmaynec@users.noreply.github.com>
2026-08-05 01:05:16 +0300

    refactor: подключить desktop router

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './shared/lib/polyfills';
-import { App } from './app/App';
+import { AppRouter } from './app/router/AppRouter';
 import './shared/styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>,
 );
